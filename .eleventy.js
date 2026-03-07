3fdcb5a138445bc7b3c72eb2139e970d97027c62
Madalina Chirpicinic <madalina.chirpicinic@gmail.com>
2026-03-07 17:41:48 +0200

    Integrate Decap CMS (#27)

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   // Pass through static assets
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/admin");
 
   return {
     dir: {
